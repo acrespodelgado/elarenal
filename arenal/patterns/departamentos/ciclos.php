2e80b90cb713f681bff61915e78621710f96313e
acrespodelgado <adrian.crespodelgado@alum.uca.es>
2026-02-24 12:55:23 +0100

    Función para añadir enlaces
    - Se ha añadido código para enlazar automáticamente los grados en los departamentos.

--- arenal/patterns/departamentos/ciclos.php
+++ arenal/patterns/departamentos/ciclos.php
@@ -18,7 +18,27 @@
                                     <p class="d-flex align-items-center">
                                         <img src="<?php echo get_site_url(); ?>/img/adorno_oferta_educativa.png" alt="Adorno oferta educativa" class="img-fluid decorator">
                                         <span class="ms-3">
-                                            <b class="accent"><?php echo esc_html($titulo); ?></b>
+                                            <?php
+                                                $titulo_lower = strtolower($titulo);
+                                                $slug_titulo = sanitize_title($titulo);
+
+                                                // Detectar tipo de grado y generar enlace
+                                                if (stripos($titulo_lower, 'basico') !== false || stripos($titulo_lower, 'básico') !== false) {
+                                                    $slug_enlace = str_replace(['grado-basico-', 'grado-básico-'], 'cfgb-', $slug_titulo);
+                                                    echo '<a href="' . get_site_url() . '/grado/' . $slug_enlace . '"><b class="accent">' . esc_html($titulo) . '</b></a>';
+                                                } elseif (stripos($titulo_lower, 'medio') !== false) {
+                                                    $slug_enlace = str_replace('grado-medio-', 'cfgm-', $slug_titulo);
+                                                    echo '<a href="' . get_site_url() . '/grado/' . $slug_enlace . '"><b class="accent">' . esc_html($titulo) . '</b></a>';
+                                                } elseif (stripos($titulo_lower, 'superior') !== false) {
+                                                    $slug_enlace = str_replace('grado-superior-', 'cfgs-', $slug_titulo);
+                                                    echo '<a href="' . get_site_url() . '/grado/' . $slug_enlace . '"><b class="accent">' . esc_html($titulo) . '</b></a>';
+                                                } elseif (stripos($titulo_lower, 'especializacion') !== false || stripos($titulo_lower, 'especialización') !== false) {
+                                                    $slug_enlace = $slug_titulo;
+                                                    echo '<a href="' . get_site_url() . '/grado/' . $slug_enlace . '"><b class="accent">' . esc_html($titulo) . '</b></a>';
+                                                } else {
+                                                    echo '<b class="accent">' . esc_html($titulo) . '</b>';
+                                                }
+                                            ?>
                                             <?php if($descripcion) : ?>
                                                 : <?php echo esc_html($descripcion); ?>
                                             <?php endif; ?>
